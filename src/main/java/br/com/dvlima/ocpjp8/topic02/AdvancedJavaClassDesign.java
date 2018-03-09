61bc3ba82db35df8be5228320294d239c9920748
Danilo Lima <danillovinicius@gmail.com>
2018-03-09 00:56:43 -0300

    Use enumerated types including methods, and constructors in an enum type

--- src/main/java/br/com/dvlima/ocpjp8/topic02/AdvancedJavaClassDesign.java
+++ src/main/java/br/com/dvlima/ocpjp8/topic02/AdvancedJavaClassDesign.java
@@ -1,14 +1,31 @@
 package br.com.dvlima.ocpjp8.topic02;
 
 /**
- Advanced Java Class Design
-
- - Develop code that uses abstract classes and methods
- - Develop code that uses the final keyword
- - Create inner classes including static inner class, local class, nested class, and anonymous inner class
- - Use enumerated types including methods, and constructors in an enum type
- - Develop code that declares, implements and/or extends interfaces and use the @Override annotation.
- - Create and use Lambda expressions
+ * Advanced Java Class Design
+ * <p>
+ * - Develop code that uses abstract classes and methods
+ * - Develop code that uses the final keyword
+ * - Create inner classes including static inner class, local class, nested class, and anonymous inner class
+ * - Use enumerated types including methods, and constructors in an enum type
+ * - Develop code that declares, implements and/or extends interfaces and use the @Override annotation.
+ * - Create and use Lambda expressions
  */
 public class AdvancedJavaClassDesign {
 }
+
+
+//04 - Rules to remember about enums
+
+/*An enum can define a main method. This means that you can define an enum as an executable Java application.*/
+/*The enum constant list might not be followed by a semicolon, if the enum doesn' define any methods or variables*/
+/*An enum constant can define a constant specific class body and use it to override existing methods or define new
+variables and methods. */
+/*An enum implicitly extends java.lang.Enum, so it can’t extend any other class. But a class can’t explicitly
+extend java.lang.Enum. An enum can implement interface(s). */
+/*An enum can never be instantiated using the keyword new. */
+/*You can define multiple constructors in your enums. */
+/*An enum can’t define a constructor with public or protected access level. */
+/*An enum can define an abstract method. Just ensure to override it for all your enum constants. */
+/*The enum method values() returns a list of all the enum constants. */
+/*An enum can be defined as a top-level enum, or as a member or another class or interface. It can’t be defined
+local to a method. */
