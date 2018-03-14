c0c0d5c6b74fb04deae1af1c85ad6ab5b89d1b36
Danilo Lima <danillovinicius@gmail.com>
2018-03-14 02:06:54 -0300

    generics

---
+++ src/main/java/br/com/dvlima/ocpjp8/topic03/item01_generic_class/Parcel.java
@@ -0,0 +1,20 @@
+package br.com.dvlima.ocpjp8.topic03.item01_generic_class;
+
+/**
+ * Created by danilolima on 13/03/18.
+ */
+//TIP: Variable names used for type parameters
+// You can't use java keywords;
+// As per Oracle's naming conventions, 
+public class Parcel<T> {
+
+    private T t;
+
+    public void set(T t){
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+}
