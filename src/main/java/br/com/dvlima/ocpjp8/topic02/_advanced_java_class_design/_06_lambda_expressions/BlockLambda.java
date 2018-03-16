1e0e7571875bdb3c684c7a5990e92e38a647df04
Danilo Lima <danillovinicius@gmail.com>
2018-03-16 18:34:13 -0300

    Create and use Lambda expressions

---
+++ src/main/java/br/com/dvlima/ocpjp8/topic02/_advanced_java_class_design/_06_lambda_expressions/BlockLambda.java
@@ -0,0 +1,17 @@
+package br.com.dvlima.ocpjp8.topic02._advanced_java_class_design._06_lambda_expressions;
+
+public class BlockLambda {
+	interface LambdaFunction {
+		String intKind(int a);
+	}
+
+	public static void main(String[] args) {
+		LambdaFunction lambdaFunction = (int i) -> {
+			if ((i % 2) == 0)
+				return "even";
+			else
+				return "odd";
+		};
+		System.out.println(lambdaFunction.intKind(10));
+	}
+}
