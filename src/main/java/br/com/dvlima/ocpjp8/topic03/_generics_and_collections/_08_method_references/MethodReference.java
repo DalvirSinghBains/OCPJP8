21534f1ec546a91534c4cfdc5d4bb9dbb7c73d94
Danilo Lima <danillovinicius@gmail.com>
2018-03-20 09:08:08 -0300

    Use method references with Streams

---
+++ src/main/java/br/com/dvlima/ocpjp8/topic03/_generics_and_collections/_08_method_references/MethodReference.java
@@ -0,0 +1,17 @@
+package br.com.dvlima.ocpjp8.topic03._generics_and_collections._08_method_references;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReference {
+
+	static void printUpperCaseString(String str) {
+		System.out.println(str.toUpperCase());
+	}
+
+	public static void main(String[] args) {
+		List<String> strings = Arrays.asList("eenvy", "meeny", "miny", "mo");
+		strings.forEach(MethodReference::printUpperCaseString);
+	}
+
+}
