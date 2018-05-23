19776876fc4fe0bd7ab1c5b005fcce600099db30
Danilo Lima <danillovinicius@gmail.com>
2018-05-23 02:05:14 -0300

    Localization

--- src/main/java/br/com/dvlima/ocpjp8/topic12/_localization/Localization.java
+++ src/main/java/br/com/dvlima/ocpjp8/topic12/_localization/Localization.java
@@ -1,11 +1,86 @@
 package br.com.dvlima.ocpjp8.topic12._localization;
 
 /**
- Localization
-
- - Read and set the locale by using the Locale object
- - Create and read a Properties file
- - Build a resource bundle for each locale and load a resource bundle in an application
+ * Localization
+ * <p>
+ * - Read and set the locale by using the Locale object
+ * - Create and read a Properties file
+ * - Build a resource bundle for each locale and load a resource bundle in an application
  */
 public class Localization {
+
+/*
+Read and set the locale by using the Locale object
+
+ A locale represents a language, culture, or country; the Locale class in Java provides an abstraction for this concept.
+
+ Each locale can have three entries: the language, country, and variant. You can use standard codes available for
+ language and country to form locale tags. There are no standard tags for variants;
+ you can provide variant strings based on your need.
+
+ The getter methods in the Locale class—such as getLanguage(), getCountry(), and getVariant()—return codes;
+ whereas the similar methods of getDisplayCountry(), getDisplayLanguage(), and getDisplayVariant() return names.
+
+ The getDefault() method in Locale returns the default locale set in the JVM. You can change this default
+ locale to another locale by using the setDefault() method.
+
+ There are many ways to create or get a Locale object corresponding to a locale:
+
+ Use the constructor of the Locale class.
+
+ Use the forLanguageTag(String languageTag) method in the Locale class.
+
+ Build a Locale object by instantiating Locale.Builder and then call setLanguageTag() from that object.
+
+ Use the predefined static final constants for locales in the Locale class.
+*/
+
+/*
+Create and read a Properties file
+
+ A resource bundle is a set of classes or property files that help define a set of keys and map those keys to
+ locale-specific values.
+
+ The class ResourceBundle has two derived classes: PropertyResourceBundle and ListResourceBundle. You can use
+ ResourceBundle.getBundle() to get the bundle for a given locale.
+
+ The PropertyResourceBundle class provides support for multiple locales in the form of property files. For each locale,
+  you specify the keys and values in a property file for that locale. You can use only Strings as keys and values.
+
+ To add support for a new locale, you can extend the ListResourceBundle class. In this derived class, you have to
+ override the Object [][] getContents() method. The returned array must have the list of keys and values.
+ The keys must be Strings, and values can be any objects.
+
+ When passing the key string to the getObject() method to fetch the matching value in the resource bundle, make sure
+ that the passed keys and the key in the resource bundle exactly match (the keyname is case sensitive).
+ If they don’t match, you'll get a MissingResourceException.
+
+ The naming convention for a fully qualified resource bundle name is
+ packagequalifier.bundlename + "_" + language + "_" + country + "_" + (variant + "_#" | "#") + script + "-" + extensions.
+ */
+
+/*
+Build a resource bundle for each locale and load a resource bundle in an application
+
+ The process of finding a matching resource bundle is same for classes extended from ListResourceBundles as for property
+  files defined for PropertyResourceBundles.
+
+ Here is the search sequence to look for a matching resource bundle. Search starts from Step 1. If at any step the search
+ finds a match, the resource bundle is loaded. Otherwise, the search proceeds to the next step.
+
+ Step 1: The search starts by looking for an exact match for the resource bundle with the full name.
+
+ Step 2: The last component (the part separated by _) is dropped and the search is repeated with the resulting shorter
+ name. This process is repeated till the last locale modifier is left.
+
+ Step 3: The search is continued using the full name of the bundle for the default locale.
+
+ Step 4: Search for the resource bundle with just the name of the bundle.
+
+ Step 5: The search fails, throwing a MissingBundleException.
+
+ The getBundle() method takes a ResourceBundle.Control object as an additional parameter. By extending this
+ ResourceBundle.Control class and passing that object, you can control or customize the resource
+ bundle searching and loading process.
+ */
 }
--- src/main/java/br/com/dvlima/ocpjp8/topic12/_localization/_03_build_resource_bundle/LoadingAResourceBundle.java
+++ src/main/java/br/com/dvlima/ocpjp8/topic12/_localization/_03_build_resource_bundle/LoadingAResourceBundle.java
@@ -1,4 +1,82 @@
 package br.com.dvlima.ocpjp8.topic12._localization._03_build_resource_bundle;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class LoadingAResourceBundle {
+    /*
+        on the oCpJp 8 exam, you’re not expected to memorize language codes or country codes that are used for naming
+        resource bundles. however, you are expected to remember the naming convention and recognize the constituents of
+        a fully qualified resource bundle name.
+     */
+
+    //packagequalifier.bundlename + "_" + language + "_" + country + "_" + (variant + "_#" | "#") + script + "-" + extensions
+
+/*
+ packagequalifier: The name of the package (or the subpackages) in which the resource bundle is provided.
+
+ bundlename: The name of the resource bundle that you’ll use in the program to refer and load it.
+
+ language: A two-letter abbreviation typically given in lowercase for the locale’s language (in rare cases, it could be three letters as well).
+
+ country: A two-letter abbreviation typically given in uppercase for the locale’s country (in rare cases, it could be three letters as well).
+
+ variant: An arbitrary list of variants (in lowercase or uppercase) to differentiate locales when you need more
+ than one locale for a language and country combination.
+*/
+
+/*
+ResourceBundle.properties               //-- Global bundle
+ResourceBundle_ar.properties            //-- Arabic language bundle
+ResourceBundle_en.properties            //-- English bundle (assuming en_US is the default locale)
+ResourceBundle_it.properties            //-- Italian language bundle
+ResourceBundle_it_IT_Rome.properties    //-- Italian (Italy, Rome) bundle
+*/
+
+}
+
+// Extend ResourceBundle.Control and override getCandidateLocales method
+// to get the list of candidate locales that Java searches for
+class TalkativeResourceBundleControl extends ResourceBundle.Control {
+    // override the default getCandidateLocales method to print
+    // the candidate locales first
+    public List<Locale> getCandidateLocales(String baseName, Locale locale) {
+        List<Locale> candidateLocales = super.getCandidateLocales(baseName, locale);
+        System.out.printf("Candidate locales for base bundle name %s and locale %s %n",
+                baseName, locale.getDisplayName());
+        candidateLocales.forEach(System.out::println);
+        return candidateLocales;
+    }
+}
+
+class CandidateLocales {
+    public static void loadResourceBundle(String resourceBundleName, Locale locale) {
+        // Pass an instance of TalkativeResourceBundleControl
+        // to print candidate locales
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(resourceBundleName,
+                locale, new TalkativeResourceBundleControl());
+        String rbLocaleName = resourceBundle.getLocale().toString();
+        // if the resource bundle locale name is empty,
+        // it means default property file
+        if (rbLocaleName.equals("")) {
+            System.out.println("Loaded the default property file with name: "
+                    + resourceBundleName);
+        } else {
+            System.out.println("Loaded the resource bundle for the locale: "
+                    + resourceBundleName + "." + rbLocaleName);
+        }
+    }
+
+    public static void main(String[] args) {
+        // trace how ResourceBundle_it_IT_Rome.properties is resolved
+        loadResourceBundle("ResourceBundle", new Locale("it", "IT", "Rome"));
+/*
+Candidate locales for base bundle name ResourceBundle and locale Italian (Italy, Rome)
+it_IT_Rome
+it_IT
+it
+Loaded the resource bundle for the locale: ResourceBundle.it_IT_Rome
+*/
+    }
 }
